ae2f92d6fe52ec8298462dfd48cc2cae775f247b
Talyslan Canabarro <78499700+Talyslan@users.noreply.github.com>
2023-08-14 21:23:43 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/divulgacao-amo/',
+  base: '/<divulgacao-amo>/',
   plugins: [react()],
 })
